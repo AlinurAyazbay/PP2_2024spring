13e10996ce2744d2f7836bfb6def8dfe7a420ea1
Alinur <ayazbay.alinur06@gmail.com>
2024-01-24 15:46:22 +0600

    message

---
+++ lab2.py/List.py
@@ -0,0 +1,33 @@
+ruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+
+    if 5 != 10:
+         print("5 and 10 is not equal")
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
